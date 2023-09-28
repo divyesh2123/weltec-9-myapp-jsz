bec48645faec6d4d37343fc392687bc0ecd907d4
divyesh2123 <divyesh2198@gmail.com>
2023-09-28 10:50:34 +0530

    This is the changes for the formhook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
+import { useForm } from 'react-hook-form';
 
 import Basic from './Basic';
 import MyEmp from './MyEmp';
 
 function App() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return (
-    <div className="App">
-      <MyEmp/>
-    </div>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <input {...register('firstName')} />
+    <input {...register('lastName', { required: true })} />
+    {errors.lastName && <p>Last name is required.</p>}
+    <input {...register('age', { pattern: /\d+/ })} />
+    {errors.age && <p>Please enter number for age.</p>}
+    <input type="submit" />
+  </form>
   );
 }
 
